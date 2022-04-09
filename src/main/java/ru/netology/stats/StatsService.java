456fcc0e5ccb468c984c95d70c7193c74cf31f20
Anton <maksakov.anton@yandex.ru>
2022-04-09 12:41:25 +0300

    run

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,62 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public int calculateSum(int[] purchases) {
+        int sum = 0;
+        for (int purchase : purchases) {
+            sum += purchase;
+        }
+        return sum;
+    }
+
+    public double calculateAverageSum(int[] purchases) {
+        double sum = calculateSum(purchases);
+        double v = sum / purchases.length;
+        return v;
+    }
+
+    public int calculateMaxMonth(int[] purchases) {
+        int maxMonth = 0;
+        for (int i = 1; i < purchases.length; i++) {
+            if (purchases[maxMonth] <= purchases[i]) {
+                maxMonth = i;
+            }
+        }
+        maxMonth += 1;
+        return maxMonth;
+    }
+
+    public int calculateMinMonth(int[] purchases) {
+        int minMonth = 0;
+        for (int i = 1; i < purchases.length; i++) {
+            if (purchases[minMonth] >= purchases[i]) {
+                minMonth = i;
+            }
+        }
+        minMonth += 1;
+        return minMonth;
+    }
+
+    public int calculateCounterUnderAverage(int[] purchases) {
+        int counter = 0;
+        double averageSum = calculateAverageSum(purchases);
+        for (int purchase : purchases) {
+            if (purchase < averageSum) {
+                counter += 1;
+            }
+        }
+        return counter;
+    }
+
+    public int calculateCounterUpperAverage(int[] purchases) {
+        int counter = 0;
+        double averageSum = calculateAverageSum(purchases);
+        for (int purchase : purchases) {
+            if (purchase > averageSum) {
+                counter += 1;
+            }
+        }
+        return counter;
+    }
+}
